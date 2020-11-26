d15f0f702a48e1370a9d64b7332e6542015e60bb
habiibaz <habiibaz1404@gmail.com>
2020-11-26 08:59:51 +0700

    make it better

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ljznsxli:d2Z7FPap1LZEUzC2d-opcI1ttZMkdGL1@suleiman.db.elephantsql.com:5432/ljznsxli'
 db = SQLAlchemy(app)
 
 from app.models import User, Tiket
--- app/models.py
+++ app/models.py
@@ -1,15 +1,16 @@
 from datetime import datetime
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.dialects.postgresql import JSON
 
 class User(db.Model):
     iduser = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    sekolah = db.Column(db.String(120), nullable=False)
-    kota = db.Column(db.String(120), nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-    ticket = db.relationship('Tiket', backref='author', lazy=True)
+    name = db.Column(JSON)
+    email = db.Column(JSON)
+    sekolah = db.Column(JSON)
+    kota = db.Column(JSON)
+    password = db.Column(JSON)
+    id_ticket = db.relationship('Tiket', backref='author', lazy=True)
 
     def __repr__(self):
         return '{} {} {} {}'.format(self.name, self.email, self.sekolah, self.kota)
@@ -22,14 +23,14 @@ class User(db.Model):
 
 class Tiket(db.Model):
     idtiket = db.Column(db.Integer, primary_key=True)
-    fullname = db.Column(db.String(20), nullable=False)
-    asalsekolah = db.Column(db.String(20), nullable=False)
-    asalkota = db.Column(db.String(20), nullable=False)
-    emailaktif = db.Column(db.String(20), nullable=False)
-    pilihanujian = db.Column(db.String(20), nullable=False)
-    univtujuan = db.Column(db.String(20), nullable=False)
-    jurusan = db.Column(db.String(20), nullable=False)
-    harga = db.Column(db.Integer, nullable=False)
+    fullname = db.Column(JSON)
+    asalsekolah = db.Column(JSON)
+    asalkota = db.Column(JSON)
+    emailaktif = db.Column(JSON)
+    pilihanujian = db.Column(JSON)
+    univtujuan = db.Column(JSON)
+    jurusan = db.Column(JSON)
+    harga = db.Column(JSON)
     status = db.Column(db.Boolean, default=False, nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     iduser = db.Column(db.Integer, db.ForeignKey('user.iduser'), nullable=False)
@@ -41,10 +42,10 @@ class Tiket(db.Model):
 
 class DetailPembayaran(db.Model):
     idpembayaran = db.Column(db.Integer, primary_key=True)
-    deskripsi = db.Column(db.String(20))
+    deskripsi = db.Column(JSON)
     totalharga = db.Column(db.Integer, default=0)
-    bank = db.Column(db.String(20))
-    norek = db.Column(db.String(30))
+    bank = db.Column(JSON)
+    norek = db.Column(JSON)
     buktitransfer = db.Column(db.String(20), default='default.jpg')
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     idtiket = db.Column(db.Integer, db.ForeignKey('tiket.idtiket'))
